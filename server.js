fb420083e742425d16b9e6fab5e75fb82a0380cb
Konrad Knitter <konrad.knitter@me.com>
2016-11-30 22:03:00 +0100

    cleanup in server.js

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
-let fs = require('fs-promise');
-let http = require('http');
+const fs = require('fs-promise');
+const http = require('http');
 
 const PORT = 3000;
 
+const HTTP_SUCCESS_OK = 200;
+const HTTP_CLIENT_NOT_FOUND = 404;
+
+const RESOURCES = {
+    "/": "www/aboutme.html",
+    "/markdown.css": "www/markdown.css"
+};
+
 async function handleRequest(request, response) {
-    console.log(request.url);
-    response.writeHead(200, {"Content-Type": "text/html"});
-    if (request.url === "/markdown.css") {
-        response.write(await fs.readFile("www/markdown.css", "utf8"));	
+    console.log(request.method + ": " + request.url);
+
+    if (request.method === "GET" && RESOURCES.hasOwnProperty(request.url)) {
+        response.statusCode = HTTP_SUCCESS_OK;
+        response.setHeader("Content-Type", "text/html");
+        response.write(await fs.readFile(RESOURCES[request.url], "utf8"));
+    } else {
+        response.writeHead(HTTP_CLIENT_NOT_FOUND);
     }
-    response.write(await fs.readFile("www/aboutme.html", "utf8"));
+
     response.end();
 }
 
-var server = http.createServer(handleRequest);
-
-server.listen(PORT, function() {
-    console.log("Something happens 2!");
-});
+http.createServer(handleRequest)
+    .listen(PORT, function() {
+        console.log("HTTP Server on port " + PORT + " started!");
+    });
