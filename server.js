9652778ceb65ef41ba429485da99528280b81c54
Konrad Knitter <konrad.knitter@me.com>
2017-01-24 21:43:51 +0100

    config.staticFiles is doing his job now. Simplification of website.js by usage of fs-extra library

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const config = require('config');
 const http = require('http');
-const fs = require('fs-promise');
+const fs = require('fs-extra-promise');
 const mime = require('mime-types');
 const HTTP_SUCCESS_OK = 200;
 const HTTP_CLIENT_NOT_FOUND = 404;
@@ -12,11 +12,11 @@ async function handleRequest(request, response) {
         response.statusCode = HTTP_SUCCESS_OK;
         if (request.url === "/") {
             response.setHeader("Content-Type", mime.lookup("html"));
-            response.write(await fs.readFile(config.outputPath + "/index.html", "utf8"));
+            response.write(await fs.readFileAsync(config.outputPath + "/index.html", "utf8"));
         } else {
             try {
                 response.setHeader("Content-Type", mime.lookup(config.outputPath + request.url));
-                response.write(await fs.readFile(config.outputPath + request.url, "utf8"));
+                response.write(await fs.readFileAsync(config.outputPath + request.url, "utf8"));
             } catch (err) {
                 console.log("File do not exists. Error 404");
                 response.writeHead(HTTP_CLIENT_NOT_FOUND);
--- website.js
+++ website.js
@@ -1,51 +1,29 @@
 "use strict"
 
 const config   = require("config");
-const fs       = require("fs-promise");
+const fs       = require("fs-extra-promise");
 const marked   = require("marked");
 
-async function createDirectory(directoryName) {
-    try {
-        let fileStat = await fs.stat(directoryName);
-        if (fileStat && !fileStat.isDirectory()) {
-            console.log(directoryName + "path exists and it is not directory.");
-        }
-    } catch (err) {
-        if (err.code == "ENOENT") {
-            fs.mkdir(directoryName);
-        } else {
-            throw err;
-        }
-l    }
-}
-
-async function copyStaticFiles() {
-    var STATIC_FILES = [ "app.js", "index.html", "jquery.min.js", "bootstrap.min.css" ];
-
-    STATIC_FILES.forEach(async function(fileName) {
-        await fs.writeFile(config.outputPath + fileName,
-            await fs.readFile(config.sourcePath + fileName),
-            "utf8");
-    });
-}
-
 async function renderPage(fileName) {
-    var htmlOutput = marked(await fs.readFile(fileName, "utf8"));
+    var htmlOutput = marked(await fs.readFileAsync(fileName, "utf8"));
     return htmlOutput;
 }
 
 async function generateMarkdownFiles() {
     var STATIC_FILES = [ 'about', 'projects', 'posts' ];
     STATIC_FILES.forEach(async function(fileName) {
-        await fs.writeFile(config.outputPath + fileName + ".html", await renderPage(config.sourcePath + fileName + ".md"), "utf8");
+        await fs.writeFileAsync(config.outputPath + fileName + ".html", await renderPage(config.sourcePath + fileName + ".md"), "utf8");
     });
 }
 
 async function main() {
     console.log("knitter.space project started");
     
-    await createDirectory(config.outputPath);
-    await copyStaticFiles();
+    await fs.ensureDirAsync(config.outputPath);
+    await Promise.all(config.staticFiles.map(function(fileName){
+        console.log(config.outputPath + fileName);
+        return fs.copyAsync(config.sourcePath + fileName, config.outputPath + fileName);
+    }));
     await generateMarkdownFiles();
 
     console.log("knitter.space project generated");
